e21ff9f03ddfda857cd9242a2319e1f338dc1c0d
Yossef Elshafey <yssef0472@gmail.com>
2023-06-11 01:45:31 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Img1 from "./assets/images/icon-reaction.svg";
+import Img2 from "./assets/images/icon-memory.svg";
+import Img3 from "./assets/images/icon-verbal.svg";
+import Img4 from "./assets/images/icon-visual.svg";
 function App() {
+  const arr = [
+    { text: "Reaction", result: 80 },
+    { text: "Memory", result: 92 },
+    { text: "Verbal", result: 61 },
+    { text: "Visual", result: 72 },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="wrapper  ">
+      <div className="left-side ">
+        <h4 className="text-white/80 text-center ">Your Result</h4>
+        <div className="circle ">
+          <p className="text-white text-3xl">76</p>
+          <p className="text-white/50">of 100</p>
+        </div>
+        <div>
+          <h3 className="text-white text-center text-2xl mb-2">Great</h3>
+          <p className="text-white text-center ">
+            You scored higher than 65% of the people who have taken these tests
+          </p>
+        </div>
+      </div>
+      <div className="right-side ">
+        <h4 className="p-4 font-bold text-2xl">Summary</h4>
+        {arr.map((e, i) => {
+          return (
+            <p key={i} className={`results ${e.text.toLowerCase()}`}>
+              <img src={`Img${i + 1}`} alt="" />
+              {e.text}
+              <span className="ml-auto text-black">
+                <span className="font-bold">{e.result}</span> / 100
+              </span>
+            </p>
+          );
+        })}
+
+        <button className="text-white bg-slate-700 py-2 rounded-3xl">
+          Continue
+        </button>
+      </div>
+    </section>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: "HankenGrotesk",
+      },
+    },
+  },
+  plugins: [],
+};
